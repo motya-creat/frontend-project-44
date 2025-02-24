1d87b9f70c63f89a7595865474b94900253bc91a
Ngok <gwngojwegwe@gmail.com>
2025-02-24 13:43:15 +0700

    modify files

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export default function cli () {
-    const name = readlineSync.question('May I have your name?')
-    return console.log('Hello,' + ' ' + name + '!')
+    const name = readlineSync.question('May I have your name? ')
+    return console.log('Hello, ' + name + '!')
 }
