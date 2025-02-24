7903e2e4dd98cb3bebb0a9af8341bb5d34d7f661
Ngok <gwngojwegwe@gmail.com>
2025-02-24 13:21:38 +0700

    add function

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
-import readlineSync from 'readline-sync';
-
-const name = readlineSync.question('Your answer: ');
+import cli from './src/cli.js'
+console.log('')
+cli()
