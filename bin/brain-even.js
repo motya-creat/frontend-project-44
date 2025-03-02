bb333775a4a2ce0245bc872ed286b4d16b2ff00e
Ngok <gwngojwegwe@gmail.com>
2025-03-03 05:15:44 +0700

    new game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,23 +1,25 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-function isEven() {  
-    randNumber = Math.floor(Math.random(10 - 1) * 10)
-    if( randNumber % 2 == 0){
-        console.log('yes')
-    }else{
-        console.log('no')
+
+const isEven = (num) => num % 2 === 0 ? 'yes' : 'no';
+
+export default function game(name) {
+    let i = 0
+    while( i < 3){
+        const randNumber = Math.floor(Math.random(10 - 1) * 10)
+        console.log(`Question: ${randNumber}`)
+        const answer = readlineSync.question('Your answer:')
+        const correct = isEven(randNumber)
+        
+        if (correct === answer) {
+            console.log('Correct')
+            i++
+        } else {
+            console.log("'yes' is wrong answer ;(. Correct answer was 'no'.")
+            console.log("Let's try again, Bill!")
+            return
+        }
     }
-}
-function game(){
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
-    console.log(`Question: ${randNumber}`)
-    const answer = readlineSync.question('answer')
-    const correct = isEven(randNumber)
-    if(correct === answer){
-        console.log('Correct')
-    }else{
-        console.log("'yes' is wrong answer ;(. Correct answer was 'no'.")
-        console.log("Let's try again, Bill!")
-    }
+    console.log(`Congratulations, Bill`)
 }
 game()
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
-import cli from './src/cli.js'
+import cli from '../src/cli.js'
+import game from '../bin/brain-even.js'
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!')
-cli()
+const name = readlineSync.question('May I have your name? ')
+console.log('Hello, ' + name + '!')
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+// cli()
