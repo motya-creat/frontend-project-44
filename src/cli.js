975ab3a44aabea707ee58a6699c07b0390194b2c
Your Name <you@example.com>
2025-03-06 13:19:54 +0700

    eslint fix

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,21 @@
 import readlineSync from 'readline-sync';
 
 export default function welcome(description) {
-    console.log('Welcome to the Brain Games!')
-    const name = readlineSync.question('May I have your name? ')
-    console.log('Hello, ' + name + '!')
-    if (description != undefined){
-        console.log(description)
-    }
-    return name
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  if (description !== undefined) {
+    console.log(description);
+  }
+  return name;
 }
-export const isEven = (num) => num % 2 === 0 ? 'yes' : 'no';
+export const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+
+export function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+export const getAnswerQestion = {
+  '+': function (x, y) { return x + y; },
+  '-': function (x, y) { return x - y; },
+  '*': function (x, y) { return x * y; },
+};
